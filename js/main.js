d364353a19169302fcd812ed347a35ebea99d087
jdelak <delakdev@gmail.com>
2019-08-29 00:44:26 +0200

    function addHero + images

--- js/main.js
+++ js/main.js
@@ -1,47 +1,45 @@
-var addRed = document.getElementById('addred');
 var playerTopCase = document.querySelectorAll("div.playertop");
- addRed.onclick = addRedDot;
 
 
-function addRedDot(){
-	var min = 0;
+ function addHero($iD){
+
+ 	$iDBouton=$iD.id;
+ 	var min = 0;
 	var max = playerTopCase.length-1;
 	var carre =  Math.floor(Math.random() * (+max - +min)) ;
-	console.log(carre);
+	var charaLvL1 = document.querySelectorAll("div."+$iDBouton+"_lvl1");
+	var charaLvL2 = document.querySelectorAll("div."+$iDBouton+"_lvl2");
+	var charaLvL3 = document.querySelectorAll("div."+$iDBouton+"_lvl3");
 
-	var playerTopRed1 = document.querySelectorAll("div.redlvl1");
-	var playerTopRed2 = document.querySelectorAll("div.redlvl2");
-	// Si carre dépasse l'index max de PlayerTopCase on initialise carre à 0;
 	if(carre == playerTopCase.length){
 		carre = 0;
 	}
 
-	// Si un rond existe dans une case on place le rond à la case suivante
-	if((playerTopCase[carre].classList.contains('redlvl1')) || (playerTopCase[carre].classList.contains('redlvl2')) || (playerTopCase[carre].classList.contains('redlvl3'))){
+	if((playerTopCase[carre].classList.contains($iDBouton+"_lvl1")) || (playerTopCase[carre].classList.contains($iDBouton+"_lvl2")) || (playerTopCase[carre].classList.contains($iDBouton+"_lvl3"))){
 		carre = carre+1;
 	}
 
-	if(playerTopRed1.length == 2){
+	if(charaLvL1.length == 2){
 
-		if(playerTopRed2.length == 2){
+		if(charaLvL2.length == 2){
 
-      playerTopRed1[0].classList.remove('redlvl1');
-      playerTopRed1[1].classList.remove('redlvl1');
-			playerTopRed2[0].classList.add('redlvl3');
-			playerTopRed2[0].classList.remove('redlvl2');
-			playerTopRed2[1].classList.remove('redlvl2');
-			playerTopRed2[2].classList.remove('redlvl2');
+      		charaLvL1[0].classList.remove($iDBouton+"_lvl1");
+      		charaLvL1[1].classList.remove($iDBouton+"_lvl1");
+			charaLvL2[0].classList.add($iDBouton+"_lvl3");
+			charaLvL2[0].classList.remove($iDBouton+"_lvl2");
+			charaLvL2[1].classList.remove($iDBouton+"_lvl2");
+			charaLvL2[2].classList.remove($iDBouton+"_lvl2");
 
 
 		}
 
-		playerTopRed1[0].classList.add('redlvl2');
-		playerTopRed1[0].classList.remove('redlvl1');
-		playerTopRed1[1].classList.remove('redlvl1');
-		playerTopRed1[2].classList.remove('redlvl1');
+		charaLvL1[0].classList.add($iDBouton+"_lvl2");
+		charaLvL1[0].classList.remove($iDBouton+"_lvl1");
+		charaLvL1[1].classList.remove($iDBouton+"_lvl1");
+		charaLvL1[2].classList.remove($iDBouton+"_lvl1");
 
 	}
 
-	playerTopCase[carre].classList.add('redlvl1');
+	playerTopCase[carre].classList.add($iDBouton+"_lvl1");
+ }
 
-}
