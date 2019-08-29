ddf62209949d9a4f011ff3ce9e2d6c298dfde511
jdelak <delakdev@gmail.com>
2019-08-29 12:00:54 +0200

    modif function addHero

--- js/main.js
+++ js/main.js
@@ -10,36 +10,42 @@ var playerTopCase = document.querySelectorAll("div.playertop");
 	var charaLvL1 = document.querySelectorAll("div."+$iDBouton+"_lvl1");
 	var charaLvL2 = document.querySelectorAll("div."+$iDBouton+"_lvl2");
 	var charaLvL3 = document.querySelectorAll("div."+$iDBouton+"_lvl3");
+  //var heros = document.querySelectorAll("hero");
 
 	if(carre == playerTopCase.length){
 		carre = 0;
 	}
 
-	if((playerTopCase[carre].classList.contains($iDBouton+"_lvl1")) || (playerTopCase[carre].classList.contains($iDBouton+"_lvl2")) || (playerTopCase[carre].classList.contains($iDBouton+"_lvl3"))){
-		carre = carre+1;
-	}
+  if(playerTopCase[carre].classList.contains("hero")){
+    carre = carre+1;
+  }
 
 	if(charaLvL1.length == 2){
 
 		if(charaLvL2.length == 2){
 
-      		charaLvL1[0].classList.remove($iDBouton+"_lvl1");
-      		charaLvL1[1].classList.remove($iDBouton+"_lvl1");
+  		charaLvL1[0].classList.remove($iDBouton+"_lvl1");
+      charaLvL1[0].classList.remove("hero");
+  		charaLvL1[1].classList.remove($iDBouton+"_lvl1");
+      charaLvL1[1].classList.remove("hero");
 			charaLvL2[0].classList.add($iDBouton+"_lvl3");
 			charaLvL2[0].classList.remove($iDBouton+"_lvl2");
 			charaLvL2[1].classList.remove($iDBouton+"_lvl2");
 			charaLvL2[2].classList.remove($iDBouton+"_lvl2");
-
-
+			charaLvL2[1].classList.remove("hero");
+			charaLvL2[2].classList.remove("hero");
+      
 		}
 
 		charaLvL1[0].classList.add($iDBouton+"_lvl2");
 		charaLvL1[0].classList.remove($iDBouton+"_lvl1");
 		charaLvL1[1].classList.remove($iDBouton+"_lvl1");
 		charaLvL1[2].classList.remove($iDBouton+"_lvl1");
+    charaLvL1[1].classList.remove("hero");
+		charaLvL1[2].classList.remove("hero");
 
 	}
 
 	playerTopCase[carre].classList.add($iDBouton+"_lvl1");
+  playerTopCase[carre].classList.add("hero");
  }
-
