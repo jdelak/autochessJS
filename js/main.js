b6dde690d6f3a18cdc0230c88f0389b04da5cf5c
jdelak <delakdev@gmail.com>
2019-08-30 13:35:15 +0200

    ajout points de vie sans css

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ function addHero($iD){
  	$iDBouton=$iD.id;
 
  	for(i = 0; i < heroArray.length; i++){
- 	
+
 	 	if($iDBouton == heroArray[i].name){
 
 		 	var min = 0;
@@ -15,7 +15,7 @@ function addHero($iD){
 			var charaLvL1 = document.querySelectorAll("div."+$iDBouton+"_lvl1");
 			var charaLvL2 = document.querySelectorAll("div."+$iDBouton+"_lvl2");
 			var charaLvL3 = document.querySelectorAll("div."+$iDBouton+"_lvl3");
-		 
+
 
 			if(carre == playerCase.length){
 				carre = 0;
@@ -29,33 +29,39 @@ function addHero($iD){
 
 				if(charaLvL2.length == 2){
 
-		  			charaLvL1[0].classList.remove($iDBouton+"_lvl1");
-		      		charaLvL1[0].classList.remove("hero");
-		  			charaLvL1[1].classList.remove($iDBouton+"_lvl1");
-		      		charaLvL1[1].classList.remove("hero");
+	  			charaLvL1[0].classList.remove($iDBouton+"_lvl1");
+      		charaLvL1[0].classList.remove("hero");
+          charaLvL1[0].innerHTML="";
+	  			charaLvL1[1].classList.remove($iDBouton+"_lvl1");
+      		charaLvL1[1].classList.remove("hero");
+          charaLvL1[1].innerHTML="";
 					charaLvL2[0].classList.add($iDBouton+"_lvl3");
 					charaLvL2[0].classList.remove($iDBouton+"_lvl2");
 					charaLvL2[1].classList.remove($iDBouton+"_lvl2");
+          charaLvL2[1].classList.remove("hero");
+          charaLvL2[1].innerHTML="";
 					charaLvL2[2].classList.remove($iDBouton+"_lvl2");
-					charaLvL2[1].classList.remove("hero");
 					charaLvL2[2].classList.remove("hero");
-		      
+					charaLvL2[2].innerHTML="";
+
 				}
 
 				charaLvL1[0].classList.add($iDBouton+"_lvl2");
 				charaLvL1[0].classList.remove($iDBouton+"_lvl1");
 				charaLvL1[1].classList.remove($iDBouton+"_lvl1");
+        charaLvL1[1].classList.remove("hero");
+        charaLvL1[1].innerHTML="";
 				charaLvL1[2].classList.remove($iDBouton+"_lvl1");
-				charaLvL1[1].classList.remove("hero");
 				charaLvL1[2].classList.remove("hero");
+				charaLvL1[2].innerHTML="";
 
 			}
 
 			playerCase[carre].classList.add($iDBouton+"_lvl1");
-		  	playerCase[carre].classList.add("hero");
-  		} 
+	  	playerCase[carre].classList.add("hero");
+      playerCase[carre].innerHTML = "<span class='health'>"+heroArray[i].health+"</span>"
+  		}
 
   	}
 
 }
-
